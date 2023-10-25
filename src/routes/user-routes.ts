c0086a0ee82e0b9e63a2b1ff4e1dc1e74fb3e9df
Lucas Barros <lucasbarros.al@gmail.com>
2023-10-25 15:02:01 -0300

    feat: signup endpoint

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,9 +3,20 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
 export async function userRoutes(server: FastifyInstance) {
   server.post('/login', (request: FastifyRequest, reply: FastifyReply) => {
-    const { email, password } = request.body as { email: any; password: any }
+    const { email } = request.body as { email: unknown; password: unknown }
     return reply
       .status(200)
       .send({ token: randomUUID(), email, name: 'Testonildo' })
   })
+
+  server.post(
+    '/login/sign-up',
+    (request: FastifyRequest, reply: FastifyReply) => {
+      const { name, email } = request.body as {
+        name: unknown
+        email: unknown
+      }
+      return reply.status(200).send({ token: randomUUID(), email, name })
+    },
+  )
 }
